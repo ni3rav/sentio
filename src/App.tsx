9118a55c01f6ae3cbad4edd255d25d59b488a6b4
ni3rav <mahetan35@gmail.com>
2025-04-13 19:38:05 +0530

    fix: purge out other assets and editor wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
+import { Textarea } from "~/components/ui/textarea";
+
 function App() {
-  return <main className="font-[var(--font-interface)]"></main>;
+  return (
+    <main className="font-[var(--font-interface)]">
+      <Textarea
+        className="h-screen w-screen rounded-none p-4"
+        style={{
+          resize: "none",
+          border: "none",
+          overflow: "auto",
+          outline: "none",
+          WebkitBoxShadow: "none",
+          MozBoxShadow: "none",
+          boxShadow: "none",
+          boxSizing: "border-box",
+          fontSize: "18px",
+          padding: "12px",
+        }}
+        placeholder="Start typing..."
+      />
+    </main>
+  );
 }
 export default App;
