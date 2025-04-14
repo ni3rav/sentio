f232e6222eab7cca9111217ccb7bf08f723f1074
ni3rav <mahetan35@gmail.com>
2025-04-14 15:17:06 +0530

    fix: switched to normal textarea for the classname fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
-import { Textarea } from "~/components/ui/textarea";
+import type React from "react";
+import { useEffect, useState } from "react";
+import { FontSizeType, ThemeType, FontType } from "~/lib/types";
 
-function App() {
+export default function App() {
+  const [text, setText] = useState("");
+  const [font, setFont] = useState<FontType>("sans");
+  const [fontSize, setFontSize] = useState<FontSizeType>("base");
+  const [themeStyle, setThemeStyle] = useState<ThemeType>("sepia");
+
+  useEffect(() => {
+    const persistedText = localStorage.getItem("sentio-content");
+    if (persistedText) {
+      setText(persistedText);
+    }
+  }, []);
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = e.target.value;
+    setText(newText);
+    localStorage.setItem("sentio-content", newText);
+  };
+
+  const textareaClasses = [
+    "h-screen",
+    "w-screen",
+    "rounded-none",
+    "overflow-hidden",
+    font === "mono"
+      ? "font-mono"
+      : font === "serif"
+      ? "font-serif"
+      : "font-sans",
+    fontSize === "xs"
+      ? "text-sm"
+      : fontSize === "sm"
+      ? "text-base"
+      : fontSize === "lg"
+      ? "text-2xl"
+      : fontSize === "xl"
+      ? "text-4xl"
+      : "text-xl",
+    themeStyle === "sepia"
+      ? "bg-amber-100 text-gray-800"
+      : themeStyle === "dark"
+      ? "bg-neutral-900 text-cyan-50"
+      : "bg-cyan-50 text-gray-950",
+  ].join(" ");
+
   return (
-    <main className="font-[var(--font-interface)]">
-      <Textarea
-        className="h-screen w-screen rounded-none p-4"
+    <main>
+      <textarea
+        spellCheck={false}
+        autoComplete="off"
+        autoCorrect="off"
+        autoCapitalize="off"
+        value={text}
+        onChange={handleTextChange}
+        className={textareaClasses}
         style={{
           resize: "none",
           border: "none",
           overflow: "auto",
           outline: "none",
-          WebkitBoxShadow: "none",
-          MozBoxShadow: "none",
           boxShadow: "none",
           boxSizing: "border-box",
-          fontSize: "18px",
-          padding: "12px",
+          padding: "18px",
         }}
-        placeholder="Start typing..."
+        placeholder="write away..."
       />
     </main>
   );
 }
-export default App;
---
+++ src/lib/types.ts
@@ -0,0 +1,3 @@
+export type FontType = "sans" | "serif" | "mono";
+export type FontSizeType = "xs" | "sm" | "base" | "lg" | "xl";
+export type ThemeType = "light" | "dark" | "sepia";
